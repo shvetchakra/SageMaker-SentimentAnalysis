4d4f5ef3f77ad3298610bb286b3675888b29a7f8
shvetchakra <shvetchakra@gmail.com>
2020-11-24 18:41:08 +0530

    Final Submission
    Fixed the issue in predict.py,

--- predict.py
+++ predict.py
@@ -85,7 +85,9 @@ def predict_fn(input_data, model):
 
     # TODO: Compute the result of applying the model to the input data. The variable `result` should
     #       be a numpy array which contains a single integer which is either 1 or 0
-
-    result = model.predict(data)
+    with torch.no_grad():
+        output = model(data)
+    result = np.round(output.numpy())
+    #result = model.predict(data)
 
     return result
